7a1dfc7b1a01f363d7584b0fd8e63c4b59438a68
Viacheslav Yemets <viacheslav.emets@gmail.com>
2021-10-18 07:24:14 +0300

    view advanced information of authorized user in profile page

--- src/UI/Content/Profile/Profile.jsx
+++ src/UI/Content/Profile/Profile.jsx
@@ -44,12 +44,38 @@ export const Profile = (props) => {
                 <AddNewPostFormRedux onSubmit={onSubmit} />
             </div>
             <div>
-                <img src={props.myProfile.photos.large} alt=" " />
-                <input type="file" onChange={onPhotoChange}/>
+                <ProfileInfo onPhotoChange={onPhotoChange} myProfile={props.myProfile}/>
             </div> 
             <div>
                 {props.myPosts}
             </div>
         </>
     )
+}
+
+let ProfileInfo = (props) => {
+    return(
+        <>
+            <img src={props.myProfile.photos.large} alt=" " />
+            <input type="file" onChange={props.onPhotoChange}/>
+            <div>
+                <b>Looking For A Job: </b>{props.myProfile.lookingForAJob || "No"}
+            </div>
+            <div>
+                <b>Looking For A Job Description: </b>{props.myProfile.lookingForAJobDescription || "No"}
+            </div>
+            <div>
+                <b>Full Name: </b>{props.myProfile.fullName || "No"}
+            </div>
+            <div>
+                <b>Contacts: </b> {Object.keys(props.myProfile.contacts).map( key => {
+                    return <Contacts contactTitle={key} contactValue={props.myProfile.contacts[key]}/>
+                })}
+            </div>
+
+        </>
+    )
+}
+let Contacts = ({contactTitle, contactValue}) => {
+    return <div><b> - {contactTitle}: </b> {contactValue}</div>
 }
